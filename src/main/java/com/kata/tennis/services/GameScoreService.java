31bc3bcbc9c4245a2879932e97d217ee0258d05d
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-09-30 14:31:24 +0200

    Implementation: check if is deuce game score

--- src/main/java/com/kata/tennis/services/GameScoreService.java
+++ src/main/java/com/kata/tennis/services/GameScoreService.java
@@ -6,14 +6,18 @@ import com.kata.tennis.model.Player;
 import java.util.Optional;
 
 public class GameScoreService {
-    public static Optional<Player> gameWinner (GameScore playerOneScore, GameScore playerTwoScore) {
+    public static Optional<Player> gameWinner(GameScore playerOneScore, GameScore playerTwoScore) {
         if (GameScore.FORTY.equals(playerOneScore))
             return Optional.of(Player.PLAYER_ONE);
         if (GameScore.FORTY.equals(playerTwoScore))
             return Optional.of(Player.PLAYER_TWO);
         return Optional.empty();
     }
+
     public static boolean isDeuce(GameScore playerOneScore, GameScore playerTwoScore) {
+        if (GameScore.FORTY.equals(playerOneScore) && GameScore.FORTY.equals(playerTwoScore) ||
+                GameScore.ADVANTEGE.equals(playerOneScore) && GameScore.ADVANTEGE.equals(playerTwoScore))
+            return true;
         return false;
     }
 }
