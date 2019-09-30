b087847551ae3a50802bb69a8fcc74d180ea9386
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-09-30 17:45:56 +0200

    Implementation: should return no winner when both are less than 6 games

--- src/main/java/com/kata/tennis/services/SetScoreService.java
+++ src/main/java/com/kata/tennis/services/SetScoreService.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 public class SetScoreService {
 
     public static Optional<Player> setWinner(int playerOneScore, int playerTwoScore) {
+        if (playerOneScore < 6 && playerTwoScore < 6)
+            return Optional.empty();
         return Optional.empty();
     }
 }
