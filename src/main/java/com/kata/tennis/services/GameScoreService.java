d74b9b4dd7eebf2dbd8e34711d0281dfc38b3ab1
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-09-30 14:16:56 +0200

    Implementation: when one of the player reach score forty

--- src/main/java/com/kata/tennis/services/GameScoreService.java
+++ src/main/java/com/kata/tennis/services/GameScoreService.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 
 public class GameScoreService {
     public static Optional<Player> gameWinner (GameScore playerOneScore, GameScore playerTwoScore) {
+        if (GameScore.FORTY.equals(playerOneScore))
+            return Optional.of(Player.PLAYER_ONE);
+        if (GameScore.FORTY.equals(playerTwoScore))
+            return Optional.of(Player.PLAYER_TWO);
         return Optional.empty();
     }
 }
