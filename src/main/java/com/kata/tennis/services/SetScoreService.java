46414862dfae2725d7be603255b209c072d15ab6
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-09-30 18:07:11 +0200

    Implementation: should return winner if he scores at least 6 within 2 more than opponent

--- src/main/java/com/kata/tennis/services/SetScoreService.java
+++ src/main/java/com/kata/tennis/services/SetScoreService.java
@@ -4,11 +4,22 @@ import com.kata.tennis.model.Player;
 
 import java.util.Optional;
 
+import static java.lang.Math.abs;
+
 public class SetScoreService {
 
     public static Optional<Player> setWinner(int playerOneScore, int playerTwoScore) {
-        if (playerOneScore < 6 && playerTwoScore < 6)
+        int setScoreDifference = abs(playerOneScore - playerTwoScore);
+        if (playerOneScore < 6 && playerTwoScore < 6) {
             return Optional.empty();
+        } else {
+            if (setScoreDifference >= 2) {
+                if (playerOneScore > playerTwoScore)
+                    return Optional.of(Player.PLAYER_ONE);
+                if (playerTwoScore > playerOneScore)
+                    return Optional.of(Player.PLAYER_TWO);
+            }
+        }
         return Optional.empty();
     }
-}
+    }
