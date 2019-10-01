0fa9c17012e818d95066fb741e7966f51d65b89b
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-10-01 14:06:40 +0200

    Implementation: should update match score when there is a set winner

--- src/main/java/com/kata/tennis/services/TennisGame.java
+++ src/main/java/com/kata/tennis/services/TennisGame.java
@@ -18,20 +18,26 @@ public class TennisGame {
     public final String playerTwoName;
     public List<ScoreHolder> setsScore = new ArrayList<>();
     public ScoreHolder currentGameScore = new ScoreHolder(0, 0);
+    public ScoreHolder currentMatchScore = new ScoreHolder(0, 0);
     public MatchStatus matchStatus = MatchStatus.IN_PROGRESS;
 
     public static TennisGame score(Player playerWhoScored, TennisGame tennisGameCurrentStatus) {
         GameScore playerOneGameScore = GameScore.getByValue(tennisGameCurrentStatus.getCurrentGameScore().getPlayerOneScore());
         GameScore playerTwoGameScore = GameScore.getByValue(tennisGameCurrentStatus.getCurrentGameScore().getPlayerTwoScore());
         if (GameScoreService.gameWinner(playerOneGameScore, playerTwoGameScore, playerWhoScored).isPresent()) {
-            tennisGameCurrentStatus.setCurrentGameScore(new ScoreHolder(0,0));
+            tennisGameCurrentStatus.setCurrentGameScore(new ScoreHolder(0, 0));
             tennisGameCurrentStatus.setCurrentSetScore(SetScoreService.score(
                     tennisGameCurrentStatus.getCurrentSetScore().getPlayerOneScore(),
-                    tennisGameCurrentStatus.getCurrentSetScore().getPlayerOneScore(), playerWhoScored)
+                    tennisGameCurrentStatus.getCurrentSetScore().getPlayerTwoScore(), playerWhoScored)
             );
         } else {
             ScoreHolder scoreHolder = GameScoreService.score(playerOneGameScore, playerTwoGameScore, playerWhoScored);
             tennisGameCurrentStatus.setCurrentGameScore(scoreHolder);
+        }
+        if (SetScoreService.setWinner(tennisGameCurrentStatus.getCurrentSetScore().getPlayerOneScore(), tennisGameCurrentStatus.getCurrentSetScore().getPlayerTwoScore()).isPresent()) {
+            tennisGameCurrentStatus.setCurrentMatchScore(MatchScoreService.
+                    score(tennisGameCurrentStatus.getCurrentMatchScore().getPlayerOneScore(),
+                    tennisGameCurrentStatus.getCurrentMatchScore().getPlayerTwoScore(), playerWhoScored));
         }
         return tennisGameCurrentStatus;
     }
